3ca1178419c11286ead5b01aa54b12445e0d163d
jamarten291 <146338884+jamarten291@users.noreply.github.com>
2026-04-13 15:21:00 +0200

    improve cover logic in product detail with linked signal

--- src/app/domains/products/pages/product-detail/product-detail.ts
+++ src/app/domains/products/pages/product-detail/product-detail.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal, OnInit, input } from '@angular/core';
+import { Component, inject, signal, OnInit, input, linkedSignal } from '@angular/core';
 import { Product } from '@shared/models/product.model';
 import { ProductService } from '@shared/services/product.service';
 import { CurrencyPipe, NgOptimizedImage } from '@angular/common';
@@ -14,7 +14,15 @@ import { CartService } from '@shared/services/cart.service';
 export default class ProductDetail implements OnInit {
   readonly slug = input<string>();
   product = signal<Product | null>(null);
-  cover = signal('');
+  cover = linkedSignal({
+    source: this.product,
+    computation: (product, previousValue) => {
+      if (product && product.images.length > 0) {
+        return product.images[0];
+      }
+      return previousValue?.value || '';
+    },
+  });
   cartService = inject(CartService);
 
   private productService = inject(ProductService);
@@ -23,17 +31,9 @@ export default class ProductDetail implements OnInit {
     const productSlug = this.slug();
     if (productSlug) {
       this.productService.getOneBySlug(productSlug).subscribe({
-        next: (product) => this.handleProduct(product),
+        next: (product) => this.product.set(product),
         error: (err) => console.error(err),
       });
-    }
-  }
-
-  handleProduct(product: Product) {
-    this.product.set(product);
-    const imageArray = product.images;
-    if (imageArray) {
-      this.cover.set(imageArray[0]);
     }
   }
 
