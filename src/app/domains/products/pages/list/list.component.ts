b2df6b76117a003abf5388d6a6455cf659de7965
jamarten291 <146338884+jamarten291@users.noreply.github.com>
2026-04-14 13:58:14 +0200

    migrate to rxResource for improved reactivity

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,11 +1,11 @@
-import { Component, inject, signal, OnInit, computed } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Product } from '@shared/models/product.model';
 import { ProductComponent } from '@products/components/product/product.component';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 import { CategoryService } from '@shared/services/category.service';
-import { Category } from '@shared/models/category.model';
 import { RouterLink } from '@angular/router';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-list',
@@ -13,41 +13,20 @@ import { RouterLink } from '@angular/router';
   templateUrl: './list.html',
   styleUrl: './list.css',
 })
-export default class List implements OnInit {
+export default class List {
   private cartService = inject(CartService);
   private productService = inject(ProductService);
   private categoryService = inject(CategoryService);
 
-  products = signal<Product[]>([]);
-  categories = signal<Category[]>([]);
-  currentCategoryId = signal<number | null>(null);
-  filteredProducts = computed(() => {
-    const categoryId = this.currentCategoryId();
-    const products = this.products();
-    if (categoryId) {
-      return products.filter((product) => product.category.id === categoryId);
-    }
-    return products;
+  categories = rxResource({
+    stream: () => this.categoryService.getCategories(),
   });
-
-  ngOnInit() {
-    this.fetchCategories();
-    this.fetchProducts();
-  }
-
-  fetchProducts() {
-    this.productService.getProducts().subscribe({
-      next: (response) => this.products.set(response),
-      error: (err) => console.error(err),
-    });
-  }
-
-  fetchCategories() {
-    this.categoryService.getCategories().subscribe({
-      next: (response) => this.categories.set(response),
-      error: (err) => console.error(err),
-    });
-  }
+  currentCategoryId = signal<number | null>(null);
+  products = rxResource({
+    params: () => ({ categoryId: this.currentCategoryId() }),
+    // TODO fix problem with products not loading when switching category for the first time, but works on subsequent switches
+    stream: ({ params }) => this.productService.getProducts(String(params.categoryId)),
+  });
 
   addToCart(product: Product) {
     this.cartService.addToCart(product);
