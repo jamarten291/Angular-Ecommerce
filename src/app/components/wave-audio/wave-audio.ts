15f7079870a81512759de7d2996bc5c68758a159
jamarten291 <146338884+jamarten291@users.noreply.github.com>
2026-04-15 13:00:28 +0200

    fix(wave-audio): usage of client js functions in the server side

--- src/app/components/wave-audio/wave-audio.ts
+++ src/app/components/wave-audio/wave-audio.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, AfterViewInit, input, viewChild } from '@angular/core';
+import { Component, ElementRef, input, viewChild, afterNextRender } from '@angular/core';
 import WaveSurfer from 'wavesurfer.js';
 import { signal } from 'wavesurfer.js/dist/reactive/store.js';
 
@@ -8,21 +8,23 @@ import { signal } from 'wavesurfer.js/dist/reactive/store.js';
   templateUrl: './wave-audio.html',
   styleUrl: './wave-audio.css',
 })
-export class WaveAudio implements AfterViewInit {
+export class WaveAudio {
   readonly audioUrl = input.required<string>();
   container = viewChild.required<ElementRef<HTMLDivElement>>('wave');
 
   isPlaying = signal(false);
   private wsRef!: WaveSurfer;
 
-  ngAfterViewInit() {
-    this.wsRef = WaveSurfer.create({
-      url: this.audioUrl(),
-      container: this.container().nativeElement,
-    });
+  constructor() {
+    afterNextRender(() => {
+      this.wsRef = WaveSurfer.create({
+        url: this.audioUrl(),
+        container: this.container().nativeElement,
+      });
 
-    this.wsRef.on('play', () => this.isPlaying.set(true));
-    this.wsRef.on('pause', () => this.isPlaying.set(false));
+      this.wsRef.on('play', () => this.isPlaying.set(true));
+      this.wsRef.on('pause', () => this.isPlaying.set(false));
+    });
   }
 
   playPause() {
--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, OnInit, DoCheck, OnDestroy, input, effect } from '@angular/core';
+import { Component, signal, OnDestroy, input, afterNextRender } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -6,48 +6,24 @@ import { Component, signal, OnInit, DoCheck, OnDestroy, input, effect } from '@a
   templateUrl: './counter.html',
   styleUrl: './counter.css',
 })
-export class Counter implements OnInit, DoCheck, OnDestroy {
+export class Counter implements OnDestroy {
   duration = input.required<number>();
   message = input.required<string>();
   counter = signal(0);
-  counterRef: number | undefined;
+  counterRef: number | null = null;
 
   constructor() {
-    console.log('Constructor executed.');
-
-    effect(() => {
-      const duration = this.duration();
-      console.log('Duration changed: ', duration);
+    afterNextRender(() => {
+      this.counterRef = window.setInterval(() => {
+        let counterValue = this.counter();
+        this.counter.update(() => (counterValue += 1));
+      }, 1000);
     });
   }
 
-  /*
-  ngOnChanges(changes: SimpleChanges) {
-    console.log('ngOnChanges executed.', changes);
-    const change = changes['duration'];
-
-    if (change && change.currentValue != change.previousValue) {
-      console.log('cambio detectado');
-    }
-  }
-  */
-
-  ngOnInit() {
-    console.log('ngOnInit executed.');
-    let counterValue = this.counter();
-
-    this.counterRef = window.setInterval(() => {
-      console.log('running counter');
-      this.counter.update(() => (counterValue += 1));
-    }, 1000);
-  }
-
-  ngDoCheck() {
-    console.log('ngDoCheck executed.');
-  }
-
   ngOnDestroy() {
-    window.clearInterval(this.counterRef);
-    console.log('ngOnDestroy executed.');
+    if (this.counterRef) {
+      window.clearInterval(this.counterRef);
+    }
   }
 }
