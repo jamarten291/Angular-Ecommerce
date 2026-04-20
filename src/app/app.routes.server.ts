e2fb685641c4a6a951fdb565c7588c740c29b527
jamarten291 <146338884+jamarten291@users.noreply.github.com>
2026-04-20 14:45:47 +0200

    feat(ssr): switch static routes to prerender rendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'about', // This page is static, so we prerender it (SSG)
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'category/:slug',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'product/:slug',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'locations',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
